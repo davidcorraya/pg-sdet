f3e53156f97604a4566eea35f8628ad47f8826c1
davidcorraya <davidcorraya94@gmail.com>
2023-11-14 04:33:57 +0600

    Variables and Datatype Coding Challenge

---
+++ Modules/Class1/variables_datatypes_challenge.js
@@ -0,0 +1,19 @@
+let fName = 'David';
+let age = 29;
+let isStudent = false;
+let favoriteColors = ['crimson red', 'black', 'white', 'blue'];
+let person = {
+    fName: 'David',
+    age: 29,
+    isStudent: true
+};
+
+console.log(fName);
+console.log(age);
+console.log(isStudent);
+console.log(favoriteColors);
+console.log(person);
+
+
+
+
