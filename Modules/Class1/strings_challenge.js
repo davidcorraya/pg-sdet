aa39f4adaff53b271749bf9a9061de6251f54144
davidcorraya <davidcorraya94@gmail.com>
2023-11-16 01:49:31 +0600

    String Challenge

---
+++ Modules/Class1/strings_challenge.js
@@ -0,0 +1,10 @@
+let coder = 'I am a good programmer'
+let stringType = typeof(coder)
+let stringLen = coder.length
+
+let toUpper = coder.toUpperCase()
+let toLower = coder.toLowerCase()
+let stringSearch = coder.includes('good')
+let stripWord = coder.replace('programmer', '')
+let findWord = coder.endsWith('programmer');
+let splitString = coder.split('good')
